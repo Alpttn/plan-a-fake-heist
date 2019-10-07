ee7cd317d6018596b0164535e9af20e223a65d91
Allie <allison.tpttn@gmail.com>
2019-10-07 11:53:47 -0500

    fixed build error and correct spelling

--- Program.cs
+++ Program.cs
@@ -13,7 +13,10 @@ namespace PlanYourHeist
             List<Dictionary<string, string>> team =
                 new List<Dictionary<string, string>>();
 
+            Console.Write("Bank Difficulty> ");
+                int bankDifficulty = int.Parse(Console.ReadLine()); 
 
+            Console.WriteLine();
 
             //get the first team members name
             Console.Write("Name> ");
@@ -56,28 +59,45 @@ namespace PlanYourHeist
                 string skillLevel = member["SkillLevel"]; //pull out the skill level
                 teamSkill = teamSkill + int.Parse(skillLevel); //add to the existing skill varaiable of the entire team
             }
+            //created a dictionary outside of the report. phase 6
+            Dictionary<string, int> report = new Dictionary<string, int>() {
+                { "Successes", 0},
+                { "Failures", 0}
+            };
 
             for (int i = 0; i < trialRunCount; i++) //we wrapped code below in for loop. every time through the loop i'll get a new luckvalue and bank difficulty
             {
                 Random generator = new Random(); //phase 4  gets random number
                 int luckValue = generator.Next(-10, 11); //takes minval and maxvalue
 
-                int bankDifficulty = 100; //needed to bring this down from the top as well.
-                bankDifficulty += luckValue; //add luckvalue to bankdifficulty and set the value
+                // int bankDifficulty = 100; //needed to bring this down from the top as well now we move it back up.
+                int trialRunBankDifficulty = bankDifficulty += luckValue; //add luckvalue to bankdifficulty and set the value -- then set this to a variable.
+
 
                 Console.WriteLine($"Team Skil level: {teamSkill}");
-                Console.WriteLine($"Bank Difficulty: {bankDifficulty}");
+                Console.WriteLine($"Bank Difficulty: {trialRunBankDifficulty}");
 
-                if (bankDifficulty > teamSkill) //did the team do better than the bank difficulty level?
+                if (trialRunBankDifficulty > teamSkill) //did the team do better than the bank difficulty level?
                 {
                     Console.WriteLine("Your heist failed :(");
+                    int failures = report["Failures"]; //get the failures out
+                    report["Failures"] = failures +1; //add one to it
                 }
                 else
                 {
                     Console.WriteLine("You are rich!"); //run it and see if you failed or not
+
+                    int Successes = report["Successes"];
+                    report["Successes"] = Successes +1;
                 }
             }
-
+                Console.WriteLine();
+                Console.WriteLine("Heist results");
+                Console.WriteLine("-------------");
+                Console.WriteLine($"Successes: {report["Successes"]}");
+                Console.WriteLine($"Failures: {report["Failures"]}");
+                Console.WriteLine();
+                Console.WriteLine();
 
 
 
